ed0dc7f7b255b7f7d80f6b62635b15cb38d00515
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2015-11-28 22:39:27 +0000

    Now prints module names

--- model.php
+++ model.php
@@ -1,7 +1,17 @@
+BEGIN:VCALENDAR
+PRODID:-//VOJit Inc@HackNotts//Nottingham WebCal Server//EN
+VERSION:2.0
 <?php
 
 $data = json_decode(file_get_contents('http://mobile.nottingham.ac.uk/hack/data/timetabling/2015/activities/course/DEFE15F6B6D777913C727DD801F89A9A'));
+$modules=$data->Activities->Course->Module;
 
-var_dump($data->Activities->Course->Module);
 
+foreach ($modules as $module)
+{
+    print_r($module->Description."\n");
+}
+ 
+    
 ?>
+END:VCALENDAR
