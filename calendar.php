b55483c82c0294165e42acfda42775a86797b131
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2015-11-29 06:16:53 +0000

    Now works out current academic year by itself - so only users starting year is needed

--- calendar.php
+++ calendar.php
@@ -19,11 +19,13 @@ function getGuid($userCourseName)
         }
     }
 }
+$currentAcademicYear = date('Y', strtotime("-9 months"));  //2015-16 is counted as just 2015 - works out current academic year startting 1st spet
+$userStartingYear = 2015;
+
+$userInYear = $currentAcademicYear - $userStartingYear + 1;
 
 
-
-
-$COURSEGUID = getGuid("Computer Science with Artificial Intelligence 4 year UG Full time/1");
-$url = "http://mobile.nottingham.ac.uk/hack/data/timetabling/2015/activities/course/$COURSEGUID";
+$COURSEGUID = getGuid("Computer Science with Artificial Intelligence 4 year UG Full time/$userInYear");
+$url = "http://mobile.nottingham.ac.uk/hack/data/timetabling/$currentAcademicYear/activities/course/$COURSEGUID";
 
 include 'calgen.php';
--- calgen.php
+++ calgen.php
@@ -19,7 +19,7 @@ function echoProperty($propertyKey, $propertyValue)
         echo $propertyKey . ":" . $propertyValue . "\n";
     }
 }
-$data = json_decode(file_get_contents('http://mobile.nottingham.ac.uk/hack/data/timetabling/2015/activities/course/DEFE15F6B6D777913C727DD801F89A9A'));
+$data = json_decode(file_get_contents($url));
 $modules = $data->Activities->Course->Module;
 
 
