763af83a101eda3dc5c0daa5fd2b60b7c925ff54
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2015-11-29 04:48:32 +0000

    Now makes basic iCal format

--- model.php
+++ model.php
@@ -1,17 +1,53 @@
+<?php
+header('Content-type: text/calendar; charset=utf-8');
+header('Content-Disposition: inline; filename=calendar.ics');
+?>
 BEGIN:VCALENDAR
 PRODID:-//VOJit Inc@HackNotts//Nottingham WebCal Server//EN
 VERSION:2.0
 <?php
 
+function getIcalDate($time, $incl_time = true)
+{
+    return $incl_time ? date('Ymd\THis', $time) : date('Ymd', $time);
+}
+
+function echoProperty($propertyKey, $propertyValue)
+{
+    if ($propertyKey && $propertyValue)
+    {
+        echo $propertyKey . ":" . $propertyValue . "\n";
+    }
+}
 $data = json_decode(file_get_contents('http://mobile.nottingham.ac.uk/hack/data/timetabling/2015/activities/course/DEFE15F6B6D777913C727DD801F89A9A'));
-$modules=$data->Activities->Course->Module;
+$modules = $data->Activities->Course->Module;
 
 
 foreach ($modules as $module)
 {
-    print_r($module->Description."\n");
+    $classes = $module->Activity;
+
+
+    foreach ($classes as $class)
+    {
+        $date = $class->Date;
+        $StartTime = $class->StartTime;
+        $EndTime = $class->EndTime;
+        $location = $class->Locations->Location;
+
+        echo "BEGIN:VEVENT\n";
+
+        echoProperty("DTSTART", getIcalDate(strtotime("$date->Day-$date->Month-$date->Year $StartTime->Hours:$StartTime->Minutes")));
+        echoProperty("DTEND", getIcalDate(strtotime("$date->Day-$date->Month-$date->Year $EndTime->Hours:$EndTime->Minutes")));
+
+
+        echoProperty("DTSTAMP", getIcalDate(time()));
+        echoProperty("SUMMARY", $class->Description);
+        echoProperty("LOCATION", $location->AbbreviatedName);
+        echoProperty("UID", $class->Guid);
+
+        echo "END:VEVENT\n";
+    }
 }
- 
-    
 ?>
 END:VCALENDAR
